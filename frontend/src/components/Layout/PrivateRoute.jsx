893fa429d0b119d508f5d3c5010d519dc003628d
yared aregayehu <yaredbaby21@gmail.com>
2026-05-27 15:15:03 -0500

    `Update PrivateRoute component to display a different loading message`

--- frontend/src/components/Layout/PrivateRoute.jsx
+++ frontend/src/components/Layout/PrivateRoute.jsx
@@ -3,7 +3,10 @@ import { useAuth } from "../../contexts/AuthContext";
 
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useAuth();
-  if (loading) return <div className="text-center py-10">Loading...</div>;
+  if (loading)
+    return (
+      <div className="flex justify-center items-center h-64">Loading...</div>
+    );
   return user ? children : <Navigate to="/login" />;
 };
 
