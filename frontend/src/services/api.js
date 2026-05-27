031e169964390d0d2d74f104c6e7c930a31ee688
yared aregayehu <yaredbaby21@gmail.com>
2026-05-27 15:12:08 -0500

    `Added API service with axios and authentication interceptor`

---
+++ frontend/src/services/api.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
+  headers: { "Content-Type": "application/json" },
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default api;
